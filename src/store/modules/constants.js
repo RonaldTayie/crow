9bf45b09bc64bd1bf122e0fab9c013ed2f58bd1d
Ronald T Ngwenya <42686081+RonaldTayie@users.noreply.github.com>
2022-08-07 19:15:54 +0200

    production server endpoints

--- src/store/modules/constants.js
+++ src/store/modules/constants.js
@@ -1,10 +1,10 @@
-const dev = true
+const dev = false
 
-const ip = dev?'192.168.8.106': '127.0.0.1'
-const port = '8000'
+const ip = dev?'192.168.8.106': '139.59.26.73'
+const port = dev?':8000':''
 
-const api = `http://${ip}:${port}/`
-const sock = `ws://${ip}:${port}/`
+const api = `http://${ip}${port}/`
+const sock = `ws://${ip}${port}/`
 
 export default {
     api,
