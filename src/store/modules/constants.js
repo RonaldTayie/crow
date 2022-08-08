1370c5af62f6609af7ca7718a7d7cfef7e88218c
Ronald T Ngwenya <42686081+RonaldTayie@users.noreply.github.com>
2022-08-08 21:35:23 +0200

    set wss

--- src/store/modules/constants.js
+++ src/store/modules/constants.js
@@ -4,9 +4,9 @@ const ip = dev?'192.168.8.106': 'apiproonal.co.za'
 const port = dev?':8000':''
 
 const header = dev?'http':'https'
-
+const socket = dev?'ws':'wss'
 const api = `${header}://${ip}${port}/`
-const sock = `ws://${ip}${port}/`
+const sock = `${socket}://${ip}${port}/`
 
 export default {
     api,
