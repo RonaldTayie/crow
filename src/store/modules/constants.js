b2b960b36348cf1c3a1261f04d5846ec4d0c88b6
Ronald T Ngwenya <42686081+RonaldTayie@users.noreply.github.com>
2022-08-08 21:21:21 +0200

    set https

--- src/store/modules/constants.js
+++ src/store/modules/constants.js
@@ -3,7 +3,9 @@ const dev = false
 const ip = dev?'192.168.8.106': '139.59.26.73'
 const port = dev?':8000':''
 
-const api = `http://${ip}${port}/`
+const header = dev?'http':'https'
+
+const api = `${header}://${ip}${port}/`
 const sock = `ws://${ip}${port}/`
 
 export default {
