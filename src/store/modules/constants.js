8639245cf6d29112836ddae200a12c8a2839a807
RonaldTayie <1999roniengwe@gmail.com>
2022-08-11 12:58:14 +0200

    websocket port

--- src/store/modules/constants.js
+++ src/store/modules/constants.js
@@ -5,8 +5,10 @@ const port = dev?':8000':''
 
 const header = dev?'http':'https'
 const socket = dev?'ws':'wss'
+const socketPort = dev?'':':8001'
+
 const api = `${header}://${ip}${port}/`
-const sock = `${socket}://${ip}${port}/`
+const sock = `${socket}${socketPort}://${ip}${port}/`
 
 export default {
     api,
